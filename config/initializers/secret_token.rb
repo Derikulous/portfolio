28a5e5699bd4b4958d32faf3ed4120b976ddbf57
Derik Strattan <derik.strattan@gmail.com>
2013-10-09 14:25:11 -0700

    My portfolio in Rails4. Developed with minitest.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio4::Application.config.secret_key_base = '9801116f66e172a6470b9882c4f39f2c5d1c39c8fc4973a1484bb49d75818afaa5d7d3c297bd57dfdcf0e0e797b52158f2e4117419d23ac75263ec0279cefa2f'
